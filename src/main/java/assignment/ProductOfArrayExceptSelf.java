db97c7b6dcfde2781397126115e922479194e475
Abhijeet Satyarthy <abhijeetsatyarthy01@gmail.com>
2026-09-11 23:43:54 +0530

    Add Week 4 assignment: Product of Array Except Self

---
+++ src/main/java/assignment/ProductOfArrayExceptSelf.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class ProductOfArrayExceptSelf {
+
+    static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+
+        int leftProduct = 1;
+        for (int i = 0; i < n; i++) {
+            answer[i] = leftProduct;
+            leftProduct *= nums[i];
+        }
+
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return answer;
+    }
+
+    static void printArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        printArray(productExceptSelf(nums));
+        sc.close();
+    }
+}
